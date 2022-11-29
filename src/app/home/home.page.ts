1f0afb59ff9c1ba9768cc35c30539dd7418b82ff
Rodrigo Galdino <galdinorodrigo553@gmail.com>
2022-11-28 22:17:13 -0300

    flashlight app basics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { Flashlight } from '@ionic-native/flashlight/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  isTorch = false;
+  
+  constructor(private flashlight: Flashlight) {}
+  
+  toggleLights(){
+    if(this.isTorch){
+      this.onFlashlight()
+    }else{
+      this.offFlashlight()
+    }
+  }
+  
+  async onFlashlight(){
+    if(await this.flashlight.available()){
+      this.isTorch = false;
+      this.flashlight.switchOn();
+    }else{
+      alert("Flashlight Not Available");
+    }
+  }
+  offFlashlight(){
+    this.isTorch = true;
+    this.flashlight.switchOff();
+  }
 }
